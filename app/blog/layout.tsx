f79561778c370d90db5b4c2af69bd8dee0b20364
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-12-08 11:50:47 +0530

    Create layout.tsx

---
+++ app/blog/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import "../../globals.css";
+
+export default function BlogLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col bg-background text-foreground">
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
