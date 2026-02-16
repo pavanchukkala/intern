e5dc77e1523cc0ad88f91e0e91a33f25e7b25f5f
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2026-02-16 21:01:47 +0530

    Update layout.tsx

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -3,19 +3,24 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import "../globals.css";
 
-
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground">
       <Header />
+
       <main className="flex-1">
         {children}
       </main>
+
+      {/* 7searchppc social ad */}
+      <div className="blog-social-ad hidden lg:block">
+        <div
+          id="7SAD1569931FD995AB7"
+          data-7pub="7SAD1569931FD995AB7"
+        ></div>
+      </div>
+
       <Footer />
     </div>
-    <div className="blog-social-ad hidden lg:block">
-  <div id="7SAD1569931FD995AB7" data-7pub="7SAD1569931FD995AB7"></div>
-</div>
-
   );
 }
