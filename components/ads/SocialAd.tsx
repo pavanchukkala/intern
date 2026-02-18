7973574455462174f4cd117ba7c88e3c689ebc38
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2026-02-18 11:25:01 +0530

    Create SocialAd.tsx

---
+++ components/ads/SocialAd.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function SocialAd() {
+  return (
+    <div className="my-6">
+      <div id="7SAD1569931FD995AB7" data-7pub="7SAD1569931FD995AB7" />
+    </div>
+  );
+}
