2521796b6b5dac228cbe06d955d3b034b0257db0
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2026-02-18 11:25:25 +0530

    Create TextAd.tsx

---
+++ components/ads/TextAd.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function TextAd() {
+  return (
+    <div className="my-4 text-sm border-l-4 border-gray-300 pl-4">
+      <div id="7SAD1569931D19BE7D5" data-7pub="7SAD1569931D19BE7D5" />
+    </div>
+  );
+}
